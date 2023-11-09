01e0c17e68efcab3acccce1240a9ac403c1c689f
Skycoder42 <Skycoder42@users.noreply.github.com>
2023-11-09 06:22:16 +0000

    create dart project

---
+++ bin/etesync_fixer.dart
@@ -0,0 +1,5 @@
+import 'package:etesync_fixer/etesync_fixer.dart' as etesync_fixer;
+
+void main(List<String> arguments) {
+  print('Hello world: ${etesync_fixer.calculate()}!');
+}
---
+++ test/etesync_fixer_test.dart
@@ -0,0 +1,8 @@
+import 'package:etesync_fixer/etesync_fixer.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
